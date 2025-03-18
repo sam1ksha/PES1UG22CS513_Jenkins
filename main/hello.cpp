a7af9634589efad76ba7e1f90c16f0c32fa60c44
Samiksha M <123168624+sam1ksha@users.noreply.github.com>
2025-03-18 16:39:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Sam\n";
+  cout << "Hello, Shar\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
